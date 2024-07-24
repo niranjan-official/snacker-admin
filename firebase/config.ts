4a4960ca813564ea364f52aa2218c611b5e600a7
niranjan-official <niranjansabarinath1521@gmail.com>
2024-07-24 09:13:45 +0530

    env created

--- firebase/config.ts
+++ firebase/config.ts
@@ -4,13 +4,13 @@ import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCxOhqlLMlwAJGcF1oKwDL6hy-DpZPrTNM",
-  authDomain: "snacker-52ec5.firebaseapp.com",
-  databaseURL: "https://snacker-52ec5-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "snacker-52ec5",
-  storageBucket: "snacker-52ec5.appspot.com",
-  messagingSenderId: "169205790473",
-  appId: "1:169205790473:web:5b642029da5585a7b3c5d8"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
